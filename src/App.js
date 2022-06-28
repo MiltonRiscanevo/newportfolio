977e58a0fa992983616d9bd13319d8068d5b34a7
North Way <miltonriscanevo@hotmail.com>
2022-06-28 15:39:46 -0500

    adicionando comportamientos al scroll

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import Home from "./components/home/Home";
 import Section1 from "./components/section1/Section1";
 import ContactUs from "./components/contactus/ContactUs"
 import AboutMe from "./components/aboutMe/AboutMe";
+import Aos from "aos"
+import { useEffect } from "react";
+import "aos/dist/aos.css"
 
 function App() {
+  useEffect(() => {
+    Aos.init({duration:2000})
+  }, [])
+  
+
   return (
     <div >
-      <Home/>
-      <AboutMe/>
-      <Section1/>
-      <ContactUs/>
+      <div data-aos="slide-down">
+       <Home />
+      </div>
+      <div data-aos="flip-down">
+        <AboutMe  />
+      </div>      
+      <div data-aos="flip-up">
+       <Section1 />
+      </div>
+      <div data-aos="slide-up">
+        <ContactUs />
+      </div>
+      
     </div>
   );
 }
